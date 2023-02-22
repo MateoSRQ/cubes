ad0453b73d8f7ace9bd6f3ff4a74306c939f89d3
Mateo <sanromanxp@gmail.com>
2023-02-21 23:18:01 -0500

    wednesday

--- ppto/src/pages/app/index.tsx
+++ ppto/src/pages/app/index.tsx
@@ -1,6 +1,7 @@
 import style from './index.module.css'
 // import Cube from '../cube'
 import Header from '../header'
+import Cube from '../cube'
 import '@fontsource/archivo'
 import {ConfigProvider, Table} from 'antd';
 import {Button, Space} from 'antd';
@@ -8,8 +9,7 @@ import {DownloadOutlined, PlusCircleOutlined} from '@ant-design/icons';
 
 import cubejs from '@cubejs-client/core';
 import {CubeProvider, useCubeQuery} from '@cubejs-client/react';
-import { format, add } from 'date-fns'; 
-import { es } from 'date-fns/locale';
+
 import { useState, useEffect } from 'react'
 import numbro from 'numbro'
 import { Scrollbars } from 'react-custom-scrollbars-2';
@@ -18,16 +18,46 @@ import useDimensions from "react-use-dimensions";
 const CUBEJS_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NzY4MzMzMjd9.RE5KaVbp40TkQgnzJ0qOp-1XxAw7d-OaI_aDs25cTzU'
 const CUBEJS_API = 'http://23.254.203.210:4000/cubejs-api/v1'
 
+/*
+
 const cubejsApi = cubejs(CUBEJS_TOKEN, {
     apiUrl: CUBEJS_API,
 });
 
-
+*/
 export default () => {
-    const [data, setData] = useState<any>([])
-    const [columns, setColumns] = useState<any>([])
+    //const [data, setData] = useState<any>([])
+    //const [columns, setColumns] = useState<any>([])
     const [ref, { x, y, width, height }] = useDimensions();
-    const {resultSet, isLoading, error, progress} = useCubeQuery({
+
+
+    const cubejs_query = {
+        "measures": [
+            "Data.totalAmount"
+        ],
+        "dimensions": [
+            "Data.actividadNivel1",
+            //"Data.actividadNivel2",
+            //"Data.actividadNivel3",
+            "Data.date"
+        ],
+        "timeDimensions": [
+            {
+                "dimension": "Data.date"
+            }
+        ],
+        "order": [
+            [
+                "Data.actividadNivel1",
+                "asc"
+            ],
+            [
+                "Data.date",
+                "asc"
+            ]
+        ]
+    }
+    /*const {resultSet, isLoading, error, progress} = useCubeQuery({
         "measures": [
             "Data.totalAmount"
         ],
@@ -53,7 +83,7 @@ export default () => {
             ]
         ]
 
-    }, {cubejsApi: cubejsApi});
+    //}, {cubejsApi: cubejsApi});
 
     let _dataSource: any  = []
     let _columns = new Set()
@@ -90,7 +120,8 @@ export default () => {
             })
         }
     }
-
+    
+*/
     return (
         <ConfigProvider
             theme={{
@@ -119,13 +150,18 @@ export default () => {
                         height: (height - 200) + 'px',
                         marginLeft: '30px'
                     }}>
-                        <Table
+{/*                         <Table
                             dataSource={_dataSource}
                             columns={[... _columns]}
                             expandable={{
                                 expandedRowRender: (record) => <p style={{ margin: 0 }}>HOLA</p>,
                                 //rowExpandable: (record) => record.name !== 'Not Expandable',
                             }}
+                        /> */}
+                        <Cube 
+                            cube_api={CUBEJS_API}
+                            cube_token={CUBEJS_TOKEN}
+                            cube_query={cubejs_query}
                         />
                     </Scrollbars>
                 </div>
--- ppto/src/pages/cube/index.tsx
+++ ppto/src/pages/cube/index.tsx
@@ -2,97 +2,121 @@ import {useEffect, useState} from 'react'
 import cubejs from '@cubejs-client/core';
 import { useCubeQuery }  from '@cubejs-client/react';
 import style from './index.module.css'
-import {Table} from 'antd';
 import type {ColumnsType} from 'antd/es/table';
 import {StyleProvider} from '@ant-design/cssinjs';
+import numbro from 'numbro'
+import {ConfigProvider, Table} from 'antd';
+import {format, add } from 'date-fns'
+import { es } from 'date-fns/locale';
 
-const cubejsApi = cubejs('YOUR-CUBEJS-API-TOKEN', {
-    apiUrl: 'http://localhost:4000/cubejs-api/v1',
-});
 
-interface DataType {
-    key: React.Key;
-    name: string;
-    age: number;
-    address: string;
-    description: string;
-}
-/*
-const columns: ColumnsType<DataType> = [
-    {title: 'Name', dataIndex: 'name', key: 'name'},
-    {title: 'Age', dataIndex: 'age', key: 'age'},
-    {title: 'Address', dataIndex: 'address', key: 'address'},
-    {
-        title: 'Action',
-        dataIndex: '',
-        key: 'x',
-        render: () => <a>Delete</a>,
-    },
-];
+ const Cube = (props: any) => {
+    console.log('CUBE PROPS')
+    console.log(props)
 
-const data: DataType[] = [
-    {
-        key: 1,
-        name: 'John Brown',
-        age: 32,
-        address: 'New York No. 1 Lake Park',
-        description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
-    },
-    {
-        key: 2,
-        name: 'Jim Green',
-        age: 42,
-        address: 'London No. 1 Lake Park',
-        description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.',
-    },
-    {
-        key: 3,
-        name: 'Not Expandable',
-        age: 29,
-        address: 'Jiangsu No. 1 Lake Park',
-        description: 'This not expandable',
-    },
-    {
-        key: 4,
-        name: 'Joe Black',
-        age: 32,
-        address: 'Sydney No. 1 Lake Park',
-        description: 'My name is Joe Black, I am 32 years old, living in Sydney No. 1 Lake Park.',
-    },
-];
-*/
+    const [data, setData] = useState<any>([])
+    const [columns, setColumns] = useState<any>([])
 
-const Component = () => {
-    const [query, setQuery] = useState({
-        "measures": [
-            "Data.totalAmount"
-        ],
-        "dimensions": [
-            "Data.actividadNivel1",
-            "Data.anyo",
-            "Data.mes"
-        ],
-        "order": {
-            "Data.totalAmount": "desc"
+    const cubejsApi = cubejs(props.cube_token, {
+        apiUrl: props.cube_api,
+    });
+
+    const {resultSet, isLoading, error, progress} = useCubeQuery(props.cube_query, {cubejsApi: cubejsApi});
+
+    let _dataSource: any  = []
+    let _columns = new Set()
+
+    if (!resultSet) {
+        return null;
+    }
+    else {
+        _dataSource = resultSet?.tablePivot({
+            x: ['Data.actividad'],
+            y: ['Data.date', 'measures']
+        });
+        let columns: any = []
+        for (let i=0; i< _dataSource.length; i++) {
+            Object.keys(_dataSource[i]).forEach((key: any, index: any) => {
+                _dataSource[i].key = i;
+                let tuple  = {title: '', dataIndex: '', name: '', width: '200px', render: function(text:any, record:any, index:any) { return text}}
+                tuple.name = key
+                tuple.dataIndex = key
+                tuple.width = '200px'
+                if (key.startsWith('Data.')) {
+                    tuple.title  = key.substring(5)
+                    tuple.width = '400px'
+                }
+                else {
+                    let _name  = key.substring(0, 10)
+                    tuple.title = format(add(Date.parse(_name), {days: 1}),'LLL-yy', { locale: es })
+                    tuple.render = function(text:any, record:any, index:any) { return numbro(text).format({mantissa: 2, thousandSeparated: true})}
+                    
+                }
+                _columns.add(tuple)
+            })
         }
-    })
+    }
 
-    // const { resultSet, isLoading, error, progress } = useCubeQuery(query, {cubejsApi: cubejsApi});
-    //
-    //
-    // if (!resultSet) {
-    //     return null;
-    // }
-    // const dataSource = resultSet.tablePivot({
-    //     x: ['Data.actividadNivel1', 'Data.anyo', 'Data.mes'],
-    //     y: ['Data.totalAmount']
-    // });
-    // const columns = resultSet.tableColumns();
+    console.log('columns')
+    console.log(_columns)
 
     return (
-        // <Table columns={columns} dataSource={dataSource} />
-        <div>DATA</div>
+        <ConfigProvider
+            theme={{
+                components: {
+                    Select: {
+                        colorPrimary: 'red',
+                        lineHeight: 3
+                    },
+                    Tag: {
+                        lineHeight: 2.4,
+                        fontSize: 16
+                    }
+                },
+                token: {
+                    fontFamily: 'Archivo, sans-serif',
+                },
+            }}
+        >
+            <div className={style.component}>
+                <div className={style.container}>
+                    <div className={style.menuContainer}>
+
+                    </div>
+                    <div className={style.tableContainer} >
+                        <Table
+                            dataSource={_dataSource}
+                            columns={[... _columns]}
+                            expandable={{
+                                expandedRowRender: (record) => {
+                                    let query = {...props.cube_query}
+                                    
+                                    query.dimensions =   [                                  
+                                        "Data.actividadNivel1",
+                                        "Data.actividadNivel2",
+                                        "Data.date"
+                                    ]
+                                    
+
+                                    return (
+                                    <Cube 
+                                        cube_query={query} 
+                                        cube_api={props.cube_api} 
+                                        cube_token={props.cube_token}
+                                    />
+                                    )
+                                },
+                                //showExpandColumn: false,
+                                expandRowByClick: true
+                                //childrenColumnName: 'Data.actividadNivel1'
+                                //rowExpandable: (record) => record.name !== 'Not Expandable',
+                            }}
+                        />
+                    </div>
+                </div>
+            </div>
+        </ConfigProvider>
     )
 }
 
-export default Component;
+export default Cube;
